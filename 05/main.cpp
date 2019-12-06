d0c843339cede4aace48311286303d8dcb44806d
Noemi Ernst <noemi.ernst@outlook.com>
2019-12-06 13:56:08 +0100

    Add solution 05 part 1 and part 2

--- 05/main.cpp
+++ 05/main.cpp
@@ -10,25 +10,139 @@ using std::string;
 using std::atoi;
 using std::vector;
 using std::to_string;
+using std::cin;
 
-
-int program(vector<int> opcodes, int noun, int verb)
+vector<int> get_modes(int opcode)
 {
-    opcodes.at(1) = noun;
-    opcodes.at(2) = verb;
+    vector<int> instruction;
+    instruction.push_back(opcode % 100);
+    opcode /= 100;
+    instruction.push_back(opcode % 10);
+    opcode /= 10;
+    instruction.push_back(opcode % 10);
+    opcode /= 10;
+    instruction.push_back(opcode % 10);
+
+    return instruction;
+}
+
+
+int program(vector<int> opcodes)
+{
 
     int acc = 0;
+    int p1 = 0;
+    int p2 = 0;
 
     while(acc < opcodes.size())
     {
-        switch(opcodes.at(acc))
+        vector<int> instruction = get_modes(opcodes.at(acc));
+        int res = 0;
+
+        switch(instruction.at(0))
         {
             case 1:
-                opcodes.at(opcodes.at(acc+3)) = opcodes.at(opcodes.at(acc+1)) + opcodes.at(opcodes.at(acc+2));
+                p1 = opcodes.at(acc+1);
+                p2 = opcodes.at(acc+2);
+                if(instruction.at(1) == 0)
+                    p1 = opcodes.at(p1);
+                if(instruction.at(2) == 0)
+                    p2 = opcodes.at(p2);
+                if(instruction.at(3) == 0)
+                    opcodes.at(opcodes.at(acc+3)) = p1 + p2;
+                else
+                    opcodes.at(acc+3) = p1 + p2;
                 acc += 4;
                 break;
             case 2:
-                opcodes.at(opcodes.at(acc+3)) = opcodes.at(opcodes.at(acc+1)) * opcodes.at(opcodes.at(acc+2));
+                p1 = opcodes.at(acc+1);
+                p2 = opcodes.at(acc+2);
+                if(instruction.at(1) == 0)
+                    p1 = opcodes.at(p1);
+                if(instruction.at(2) == 0)
+                    p2 = opcodes.at(p2);
+                if(instruction.at(3) == 0)
+                    opcodes.at(opcodes.at(acc+3)) = p1 * p2;
+                else
+                    opcodes.at(acc+3) = p1 * p2;
+                acc += 4;
+                break;
+            case 3:
+                p1 = opcodes.at(acc+1);
+                if(instruction.at(1) == 0)
+                    p1 = opcodes.at(p1);
+                int input;
+                cout << "Enter Code: ";
+                cin >> input;
+                if(instruction.at(1) == 0)
+                    opcodes.at(opcodes.at(acc+1)) = input;
+                else
+                    opcodes.at(acc+1) = input;
+                acc += 2;
+                break;
+            case 4:
+                p1 = opcodes.at(acc+1);
+                if(instruction.at(1) == 0)
+                    p1 = opcodes.at(p1);
+                cout << "Output: " << p1 << endl;
+                acc += 2;
+                break;
+            case 5:
+                p1 = opcodes.at(acc+1);
+                p2 = opcodes.at(acc+2);
+                if(instruction.at(1) == 0)
+                    p1 = opcodes.at(p1);
+                if(p1 != 0)
+                {
+                    if(instruction.at(2) == 0)
+                        p2 = opcodes.at(p2);
+                    acc = p2;
+                } else
+                    acc += 3;
+                break;
+            case 6:
+                p1 = opcodes.at(acc+1);
+                p2 = opcodes.at(acc+2);
+                if(instruction.at(1) == 0)
+                    p1 = opcodes.at(p1);
+                if(p1 == 0)
+                {
+                    if(instruction.at(2) == 0)
+                        p2 = opcodes.at(p2);
+                    acc = p2;
+                } else
+                    acc += 3;
+                break;
+            case 7:
+                p1 = opcodes.at(acc+1);
+                p2 = opcodes.at(acc+2);
+                if(instruction.at(1) == 0)
+                    p1 = opcodes.at(p1);
+                if(instruction.at(2) == 0)
+                    p2 = opcodes.at(p2);
+                res = 0;
+                if(p1 < p2)
+                    res = 1;
+                if(instruction.at(3) == 0)
+                    opcodes.at(opcodes.at(acc+3)) = res;
+                else
+                    opcodes.at(acc+3) = res;
+                acc += 4;
+                break;
+            case 8:
+                p1 = opcodes.at(acc+1);
+                p2 = opcodes.at(acc+2);
+                if(instruction.at(1) == 0)
+                    p1 = opcodes.at(p1);
+                if(instruction.at(2) == 0)
+                    p2 = opcodes.at(p2);
+                res = 0;
+                if(p1 == p2)
+                    res = 1;
+                if(instruction.at(3) == 0)
+                    opcodes.at(opcodes.at(acc+3)) = res;
+                else
+                    opcodes.at(acc+3) = res;
                 acc += 4;
                 break;
             case 99:
@@ -59,31 +173,7 @@ int main() {
     }
     input_file.close();
 
-    for (size_t i = 0; i < opcodes.size(); i++)
-        cout << opcodes[i] << endl;
-
-    int output = 0;
-    int noun = 0;
-    int verb = 0;
-    for(;output != 19690720 && noun < 100; ++noun)
-    {
-        verb = 0;
-        for(;output != 19690720 && verb < 100; ++verb)
-        {
-            vector<int> inp;
-            for (size_t i = 0; i < opcodes.size(); i++)
-                inp.push_back(opcodes[i]);
-            output = program(inp, noun, verb);
-        }
-    }
-    --verb;
-    --noun;
-
-
-    cout << "Output: " << output << endl;
-    cout << "Noun: " << noun << endl;
-    cout << "Verb: " << verb << endl;
-    cout << "100 * noun + verb = " << 100*noun+verb << endl;
+    program(opcodes);
 
     return 0;
 }
